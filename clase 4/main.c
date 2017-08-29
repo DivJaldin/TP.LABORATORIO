b662a671915ddbe63c3d98915af90977aa87e7ce
aromero98 <agustinleandroromero@gmail.com>
2017-08-29 20:28:45 -0300

    Add files via upload

---
+++ clase 4/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(void)
+{
+int x,y,z;
+printf("ingrese numero a sumar: ");
+scanf("%d",&x);
+printf("ingrese numero a sumar: ");
+scanf("%d",&y);
+z=suma(x,y);
+printf("La suma es %d",z);
+}
+
